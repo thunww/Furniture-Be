df3f437e3193adf45260c79dbb3a17319fc550ce
annguyenax <annguyen11012k4@gmail.com>
2025-11-05 15:02:30 +0700

    fix search furniture

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -281,6 +281,7 @@ const getProductDetails = async (req, res) => {
 const searchProducts = async (req, res) => {
   try {
     const {
+
       q = "",
       category_id,
       min_price: minPriceRaw = "0",
--- src/controllers/vendorController.js
+++ src/controllers/vendorController.js
@@ -175,9 +175,9 @@ const handleGetShopRating = async (req, res) => {
   }
 };
 
-const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY,
-});
+// const openai = new OpenAI({
+//   apiKey: process.env.OPENAI_API_KEY,
+// });
 const handleAIChat = async (req, res) => {
   try {
     const { message } = req.body;
@@ -386,8 +386,7 @@ const handleExportOrders = async (req, res) => {
         "Tên sản phẩm": order.orderItems
           .map(
             (item) =>
-              `${item.product_name} (${item.quantity}x)${
-                item.size ? ` - ${item.size}` : ""
+              `${item.product_name} (${item.quantity}x)${item.size ? ` - ${item.size}` : ""
               }${item.color ? ` - ${item.color}` : ""}`
           )
           .join("\n"),
@@ -398,9 +397,8 @@ const handleExportOrders = async (req, res) => {
           order.orderItems[0]?.discount || 0
         ).toLocaleString("vi-VN")}%`,
         "Thành tiền": `${itemTotal[0].toLocaleString("vi-VN")}đ`,
-        "Khách hàng": `${order.customer?.username || ""} (${
-          order.customer?.phone || ""
-        })`,
+        "Khách hàng": `${order.customer?.username || ""} (${order.customer?.phone || ""
+          })`,
         Email: order.customer?.email || "",
         "Người nhận": order.shipping_address?.recipient_name || "",
         "SĐT người nhận": order.shipping_address?.phone || "",
@@ -699,14 +697,14 @@ const handleRegisterVendor = async (req, res) => {
   try {
     const userId = req.user.user_id;
     const {
-      
+
       shopName,
       description,
       address,
     } = req.body;
 
     // Kiểm tra các trường bắt buộc
-    if (!shopName || !address ) {
+    if (!shopName || !address) {
       return res.status(400).json({
         success: false,
         message: "Vui lòng điền đầy đủ thông tin bắt buộc",
@@ -717,11 +715,11 @@ const handleRegisterVendor = async (req, res) => {
     const result = await vendorService.registerVendor(
       userId,
       {
-       
+
         shopName,
         description,
         address,
-        
+
       },
       req.uploadedImages
     );
